ab2539705fba5cbd0a7d7168ff746c403861db98
R0MAINa <romain.auge@iut-rodez.fr>
2026-03-06 15:08:55 +0100

    td1

---
+++ exercices/01-javascript-basics/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+    <title>test</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<h1>Pokemon games</h1>
+<div class="main">
+    <div class="buttons">
+        <button>Open</button>
+        <button>Close</button>
+    </div>
+    <div class="content"></div>
+</div>
+</body>
+</html>
